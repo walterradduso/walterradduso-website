dccfd05618297831bcd3a17109a4e070b035d661
Walter Radduso <w.radduso@gmail.com>
2020-06-02 19:18:12 -0300

    Updated styles. Added a new experience. Added certification. Updated resume.

---
+++ precache-manifest.a5c128765944b534895182c0593b9c13.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a568db57b71e9d5ae5e5c119acbf2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1c8607a98b8dd001ec0",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "fb272ed307d59b8d94b2",
+    "url": "/static/css/main.c4122e1b.chunk.css"
+  },
+  {
+    "revision": "a1c8607a98b8dd001ec0",
+    "url": "/static/js/2.ba5bcecc.chunk.js"
+  },
+  {
+    "revision": "109083572dc3ba74f105a9f6f5c9e305",
+    "url": "/static/js/2.ba5bcecc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb272ed307d59b8d94b2",
+    "url": "/static/js/main.7da78074.chunk.js"
+  },
+  {
+    "revision": "bb9322194de2bc80562b",
+    "url": "/static/js/runtime-main.befb5f9a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e18674be486f448bc3b8b7ac924811c4",
+    "url": "/static/media/profile-img.e18674be.jpg"
+  },
+  {
+    "revision": "da3bd82649cd31d222f5db5e08aeca89",
+    "url": "/static/media/resume_walter_radduso.da3bd826.pdf"
+  }
+]);
